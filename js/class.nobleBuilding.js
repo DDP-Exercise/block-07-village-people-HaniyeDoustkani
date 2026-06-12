b843de246d9277e4d372bf9be568b3bd0f0227a2
HaniyeDoustkani <s2510892041@fhooe.at>
2026-06-12 19:28:51 +0200

    Update class.nobleBuilding.js

--- js/class.nobleBuilding.js
+++ js/class.nobleBuilding.js
@@ -4,3 +4,9 @@
  * Create a NobleBuilding class. It's basically the same as a regular building
  * with one exception: Only nobles allowed.
  */
+
+import Building from "./class.building.js";
+
+export default class NobleBuilding extends Building{
+    
+}
