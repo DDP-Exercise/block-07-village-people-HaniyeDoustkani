8a52f9e8dec2d36d249b2810efaec504c7147a9e
HaniyeDoustkani <s2510892041@fhooe.at>
2026-06-12 08:54:56 +0200

    Update class.nobleCitizen.js

--- js/class.nobleCitizen.js
+++ js/class.nobleCitizen.js
@@ -1,6 +1,20 @@
 "use strict";
+import Citizen from "./class.citizen.js";
 /**
  * Create a NobleCitizen class. A Noble Citizen is basically
  * the same as a regular citizen, just with rank 1 and another
  * representation in the Citizen Dictionary (see the img folder).
  */
+
+export default class NobelCitizen extends Citizen {
+    constructor() {
+        super(name);
+        this.rank = 1;
+
+    }
+
+    toString(){
+        return `${this.name}, Nobel Citizen` ;
+
+    }
+};
