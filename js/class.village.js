68bd31ca9fc68b215e1f9d5ce2e3c3f8fc623498
HaniyeDoustkani <s2510892041@fhooe.at>
2026-06-12 19:28:56 +0200

    Update class.village.js

--- js/class.village.js
+++ js/class.village.js
@@ -1,7 +1,10 @@
 "use strict";
 
-import NobelCitizen from "./class.nobleCitizen";
-import Citizen from "./class.citizen";
+import NobleCitizen from "./class.nobleCitizen.js";
+import Citizen from "./class.citizen.js";
+
+import Building from "./class.building.js";
+import NobleBuilding from "./class.nobleBuilding.js";
 
 /**
  * Create a Village class. Each village should have
@@ -13,40 +16,40 @@ import Citizen from "./class.citizen";
  * implement them.
  */
 
-export default class Village{
-    constructor(name){
+export default class Village {
+    constructor(name) {
         this.name = name;
         this.buildings = [];
         this.citizens = [];
     }
 
-    addBuilding(name, capacity, nobel = false){
-        this.buildings.push(nobel ? new NobelBuilding(name, capacity) : new Building(name, capacity));
-
-
-
-        if(nobel)
-            let building = new Building(name, capacity);
-        else
-            let building = new Building(name, capacity);
+    addBuilding(name, capacity, noble = false) {
+        this.buildings.push(noble ? new NobleBuilding(name, capacity) : new Building(name, capacity));
 
     }
 
+    //TODO: something els is required here.
 
-    addCitizen(name, nobel = false){
-        let citizen = nobel ? new NobelCitizen(name) : new Citizen(name);
-        //TODO: something els is required here.
+    addCitizen(name, noble = false) {
+        let citizen = noble ? new NobleCitizen(name) : new Citizen(name);
+
 
         this.citizens.push(citizen);
         this.shelterTheWorthy(citizen);
 
     }
 
-    shelterTheWorthy(){
-        for(const building of this.buildings){
-            if(building.addResident(citizen))
-                return;
-
+    shelterTheWorthy(citizen) {
+        if (citizen) {
+            for (const building of this.buildings) {
+                if (building.addResident(citizen))
+                    return;
+            }
+        } else {
+            for (const citizen of this.citizens) {
+                if (citizen.home === null)
+                    this.shelterTheWorthy(citizen);
+            }
         }
 
     }
@@ -55,17 +58,24 @@ export default class Village{
 
         //for all buildings: list your residents.
 
-        for (const building of thid.buildings) {
+        for (const building of this.buildings) {
             building.listAllResidents();
 
-            this.listAllHomeless();
 
+        }
+        this.listAllHomeless();
+
+    }
+
+    listAllHomeless() {
+        console.log(" Homeless people of" + this.name + ":", "background-color. #a00; color: white");
+
+        for (const citizen of this.citizens) {
+            if (citizen.home === null)
+                console.log(citizen.toString());
         }
     }
 
-        listAllHomeless(){
-        console.log(" Homeless people of" + this.name + ":" , "background-color. #a00; color: white");
-
-        }
 
 }
+
